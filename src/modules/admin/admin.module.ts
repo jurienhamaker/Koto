f3502523d65e40e46aac836f0aa2dbdd61f3c0f2
Jurien Hamaker <whoami@jurien.dev>
2023-10-21 00:00:32 +0200

    feat(Admin):  Add scraping capability on development env

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { SharedModule } from 'src/shared.module';
 import { GameModule } from '../game/game.module';
@@ -5,16 +6,24 @@ import { WordsModule } from '../words/words.module';
 import { AdminEmojisCommands } from './commands/emojis.commands';
 import { AdminGetWordCommands } from './commands/get-word.commands';
 import { AdminRecreateCommands } from './commands/recreate.commands';
+import { AdminScrapeWordsCommands } from './commands/scrape-words.commands';
 import { AdminSendWelcomeCommands } from './commands/send-welcome.commands';
+import { AdminScrapeService } from './services/scrape.service';
 
 @Module({
-	imports: [SharedModule, WordsModule, GameModule],
+	imports: [SharedModule, WordsModule, GameModule, HttpModule],
 	providers: [
+		AdminScrapeService,
+
 		// commands
 		AdminEmojisCommands,
 		AdminRecreateCommands,
 		AdminSendWelcomeCommands,
 		AdminGetWordCommands,
+
+		...(process.env.NODE_ENV !== 'production'
+			? [AdminScrapeWordsCommands]
+			: []),
 	],
 })
 export class AdminModule {}
