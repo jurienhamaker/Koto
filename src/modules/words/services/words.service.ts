ab08537390f035a0210a80df014c5f9e908abe94
Jurien Hamaker <whoami@jurien.dev>
2023-10-21 14:45:43 +0200

    feat(Words): Split existing & game words to make it a little easier

--- src/modules/words/services/words.service.ts
+++ src/modules/words/services/words.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
+import * as exists from '../assets/exists.json';
 import * as words from '../assets/words.json';
 
 @Injectable()
@@ -6,27 +7,24 @@ export class WordsService {
 	private readonly _logger = new Logger(WordsService.name);
 
 	private _wordsByLetter: Map<string, string[]>;
+	private _existsByLetter: Map<string, string[]>;
 
 	public amount: number = 0;
 
 	constructor() {
 		this._logger.log('Initializing words list.');
 
+		this._createMaps();
 		this.amount = words.length;
-		this._wordsByLetter = ((m, a) => (
-			a.forEach((s) => {
-				let a = m.get(s[0]) || [];
-				m.set(s[0], (a.push(s), a));
-			}),
-			m
-		))(new Map(), words);
 
-		this._logger.log('Finished initializing words list.');
+		this._logger.log(
+			`Finished initializing words list. Loaded ${exists.length} guess words & ${words.length} game words`,
+		);
 	}
 
 	exists(word: string) {
 		const firstLetter = word[0];
-		const set = this._wordsByLetter.get(firstLetter);
+		const set = this._existsByLetter.get(firstLetter);
 
 		if (!set) {
 			return false;
@@ -36,14 +34,34 @@ export class WordsService {
 		return index >= 0;
 	}
 
-	getRandom(ignored: string[] = []) {
+	getRandom(ignored: string[] = [], hard = false) {
 		const letter = this._randomLetter();
-		const words = this._wordsByLetter.get(letter);
+		const words = (hard ? this._existsByLetter : this._wordsByLetter).get(
+			letter,
+		);
 
 		const filteredWords = words.filter((w) => ignored.indexOf(w) === -1);
 
 		const randomIndex = Math.floor(Math.random() * filteredWords.length);
 		return filteredWords[randomIndex];
+	}
+
+	private _createMaps() {
+		this._wordsByLetter = ((m, a) => (
+			a.forEach((s) => {
+				let a = m.get(s[0]) || [];
+				m.set(s[0], (a.push(s), a));
+			}),
+			m
+		))(new Map(), words);
+
+		this._existsByLetter = ((m, a) => (
+			a.forEach((s) => {
+				let a = m.get(s[0]) || [];
+				m.set(s[0], (a.push(s), a));
+			}),
+			m
+		))(new Map(), [...words, ...exists]);
 	}
 
 	private _randomLetter() {
