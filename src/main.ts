76edb1bf4cd5da2ff4f280e5d8f35f618bb66f3a
Jurien Hamaker <whoami@jurien.dev>
2023-10-21 16:10:25 +0200

    refactor: Don't show debug logs in production

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { KotoModule } from './koto.module';
 async function bootstrap() {
 	const app = await NestFactory.create(KotoModule, {
 		cors: true,
+		...(process.env.NODE_ENV === 'production'
+			? {
+					logger: ['error', 'warn', 'log'],
+			  }
+			: {}),
 	});
 
 	app.setGlobalPrefix('api');
